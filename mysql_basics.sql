a4839d6d91c7828424fa690f9a3027eb28ee3419
sam <samaniloqman91@gmail.com>
2023-11-09 16:42:13 +0100

    a new update

--- mysql_basics.sql
+++ mysql_basics.sql
@@ -1,22 +1,21 @@
+-- Crete a data base
 
-# Crete a data base
+CREATE DATABASE database_name   -- create a new database.
 
-CREATE DATABASE database_name  # create a new database.
+USE database_name   -- active the database.
 
-USE database_name  # active the database.
+DROP database_name   -- delete the created database, in this case database_name.
 
-DROP database_name   # delete the created database, in this case database_name.
-
-DROP DATABASE test_database;   # delete theintire databse without using USE statement.
+DROP DATABASE test_database;   -- delete theintire databse without using USE statement.
 
 
-ALTER DATABASE database_name  READ ONLY = 1;  # the mood of database is read only and you can not modify it.
-ALTER DATABASE database_name READ ONLY = 0;    # you can modify it.
-
+ALTER DATABASE database_name  READ ONLY = 1;  -- the mood of database is read only and you can not modify it.
+ALTER DATABASE database_name READ ONLY = 0;    -- you can modify it.
 
 
 
-# create a table in the data base
+
+-- create a table in the data base
 
 
 CREATE TABLE friends (
@@ -29,52 +28,59 @@ CREATE TABLE friends (
 
 );
 
-SELECT * FROM friends;  # select the whole table.
+SELECT * FROM friends;  -- select the whole table.
 
-RENAME TABLE friends TO coworker;   # rename a table.
+RENAME TABLE friends TO coworker;   -- rename a table.
 
-DROP TABLE coworker;   # delete a table.
+DROP TABLE coworker;   -- delete a table.
 
 
 
-# modify a table
+-- modify a table
 
 ALTER TABLE coworker
 ADD phone_number VARCHAR(15);
 
 
-# rename a column from a table
+-- modify a value or a variable
+
+UPDATE customers
+SET first_name = "Loqman"
+WHERE customer_id = 1;
+
+
+-- rename a column from a table
 
 ALTER TABLE coworker
 RENAME COLUMN phone_number TO email;
 
 
-# modify the variable type in a column of a table
+-- modify the variable type in a column of a table
 
 ALTER TABLE coworker
 MODIFY COLUMN email VARCHAR(100);
 
 
-# change the position of a column in a table
+-- change the position of a column in a table
 
 ALTER TABLE coworker
 MODIFY email VARCHAR(100)
-AFTER last_name; # if you want to the column be in the first position just type FIRST;
+AFTER last_name;    -- if you want to the column be in the first position just type FIRST;
 
 
-# delete a column from a table
+-- delete a column from a table
 
 ALTER TABLE coworker
 DROP COLUMN email;
 
 
-# insert a row to a table
+-- insert a row to a table
 
 INSERT INTO coworker
 VALUES (2, "Loqman", "Samani", 30.40, "2022-03-06");
 
 
-# insert multiple rows to a table
+-- insert multiple rows to a table
 
 INSERT INTO coworker
 
@@ -86,20 +92,20 @@ VALUES (3, "Ali", "Irani", 12.49, "2002-04-08"),
 
 
 
-# insert rows with missing values  
+-- insert rows with missing values  
 
 INSERT INTO coworker (friends_id, first_name, last_name)
 VALUES (7, "Diman", "Hossaini");
 
 
-# select specific columns from a table
+-- select specific columns from a table
 
 SELECT first_name, last_name
 FROM coworker;
 
 
 
-# select a specific column from a table
+-- select a specific row from a table
 
 SELECT * FROM coworker
 WHERE friends_id = 2;
@@ -119,7 +125,7 @@ SELECT * FROM coworker
 WHERE hire_date IS NOT NULL;
 
 
-# update a column
+-- update a column
 
 ET SQL_SAFE_UPDATES = 0;
 
@@ -128,7 +134,7 @@ SET hourly_pay = 20.00
 WHERE friends_id = 7;
 
 
-# update multiple columns
+-- update multiple columns
 
 SET SQL_SAFE_UPDATES = 0;
 
@@ -146,24 +152,24 @@ WHERE friends_id = 2;
 
 
 UPDATE coworker
-SET hire_date = "2001-03-06";   # update the whole column
+SET hire_date = "2001-03-06";   -- update the whole column
 
 
 
-# delete a row
+-- delete a row
 
 DELETE FROM coworker
 WHERE friends_id = 4;
 
 
-# auto commit on and off
+-- auto commit on and off
 
 SET AUTOCOMMIT = OFF;
 
 SET AUTOCOMMIT = ON
 
 
-# COMMIT manually
+-- COMMIT manually
 
 COMMIT;
 
@@ -174,25 +180,25 @@ COMMIT;
 
 
 
-# if you delete something in autocommit = off mode, you can undo it
+-- if you delete something in autocommit = off mode, you can undo it
 
-SET AUTOCOMMIT = OFF;  # set auto safe to off
+SET AUTOCOMMIT = OFF;  -- set auto safe to off
 
-COMMIT;  # safe the data
+COMMIT;  -- safe the data
 
-DELETE FROM coworker;   # delete all points
+DELETE FROM coworker;   -- delete all points
 SELECT * FROM coworker;
 
-ROLLBACK;                 # go back to the safe point
-SELECT * FROM coworker;    # no the deleted data is restore
+ROLLBACK;                 -- go back to the safe point
+SELECT * FROM coworker;    -- no the deleted data is restore
 
 
 
 
-#  create a datetime table
+--  create a datetime table
 
 
-USE test_db;  # first select the database
+USE test_db;  -- first select the database
 
 CREATE TABLE current_date_time (
      my_date DATE,
@@ -202,7 +208,7 @@ CREATE TABLE current_date_time (
 
 SELECT * FROM current_date_time;
 
-# insert current date and time   
+-- insert current date and time   
 
 INSERT INTO current_date_time
 VALUES (
@@ -213,7 +219,7 @@ VALUES (
 
 
 
-# set a column in a table to be uniqie, which is already created
+-- set a column in a table to be uniqie, which is already created
 
 USE test_db;
 
@@ -221,7 +227,7 @@ ALTER TABLE products
 ADD CONSTRAINT
 UNIQUE (product_name);  
 
-# you can do the same when you are creating the table
+-- you can do the same when you are creating the table
 
 USE test_db;
 
@@ -235,7 +241,7 @@ VALUES (
 
 
 
-# create a column which does not accept null values
+-- create a column which does not accept null values
 
 CREATE TABLE not_null (
    can_be_null INT,
@@ -248,7 +254,7 @@ VALUES (NULL, 1.1),
 
 
 
-# check the entrance of a column
+-- check the entrance of a column
 
 CREATE TABLE check_table (
    value_to_check DECIMAL(4, 2),
@@ -257,19 +263,19 @@ CREATE TABLE check_table (
 );
 
 
-# modify version
+-- modify version
 
 ALTER TABLE check_table
 
 ADD CONSTRAINT check_col CHECK(hourly_pay >= 10.10);
 
-# delete a check statement
+--delete a check statement
 
 ALTER TABLE check_table
 DROP CHECK check_col;
 
 
-# create a table with a default column
+-- create a table with a default column
 
 USE test_db;
 CREATE TABLE products(
@@ -278,12 +284,12 @@ CREATE TABLE products(
     price DECIMAL (4, 2) DEFAULT 0.00
 );
 
-# modify version
+-- modify version
 
 ALTER TABLE products
 ALTER price SET DEFAULT 0;
 
-# add new rows
+-- add new rows
 
 SELECT * FROM products;
 
@@ -293,7 +299,7 @@ VALUES (104, "napkin"),
        (106, "pizza");
 
 
-# useful usage of default
+-- useful usage of default
 
 USE test_db;
 
@@ -315,7 +321,7 @@ SELECT * FROM transactions;
 
 
 
-# create a table with primary key
+-- create a table with primary key
 
 USE test_db;
 
@@ -336,10 +342,210 @@ VALUES (301, 1.2, "correct"),
        
 SELECT * FROM primary_key;
 
-# modify version
+-- modify version
+
 ALTER TABLE primary_key
 ADD CONSTRAINT
 PRIMARY KEY (pri_id);
+
+
+
+
+
+
+-- a good example
+
+
+CREATE TABLE transactions(
+
+    trans_id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL (5, 2),
+    data_time DATETIME DEFAULT NOW()
+);
+
+INSERT INTO transactions (amount)
+VALUES (32.22),
+       (32.66),
+       (20.00),
+       (10),
+       (22);
+       
+
+
+
+
+
+-- how to connect two tables using foreign key
+
+USE test_db;
+
+
+CREATE TABLE customers(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+
+CREATE TABLE transactions(
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL(5, 2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    date_time DATETIME DEFAULT NOW()
+);
+
+
+
+
+-- INNER JOIN (intersection)
+
+
+-- if we have two tables (transactions & customers):
+
+CREATE TABLE customers(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+
+CREATE TABLE transactions(
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL(5, 2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    date_time DATETIME DEFAULT NOW()
+);
+
+
+-- They can be connected using INNER JOIN function:
+
+SELECT * FROM transactions
+INNER JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+
+SELECT * FROM customers
+INNER JOIN transactions
+ON customers.customer_id = transactions.customer_id;
+
+
+SELECT transaction_id, first_name, last_name,  amount FROM  transactions
+INNER JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+
+
+-- LEFT & RIGHT JOIN 
+
+SELECT * FROM transactions
+LEFT JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+
+SELECT * FROM customers
+RIGHT JOIN transactions
+ON customers.customer_id = transactions.customer_id;
+
+
+SELECT transaction_id, first_name, last_name,  amount FROM  transactions
+LEFT JOIN customers
+ON transactions.customer_id = customers.customer_id;
+
+
+
+
+
+-- functions in mySQL:
+
+
+SELECT COUNT(amount) FROM transactions; -- Number of rows of amount column in transactions table (it generates a column with the name of "COUNT(amount)")
+
+SELECT COUNT(amount) AS amount_count FROM transactions; -- You can modify the name of the generated column using "AS".
+
+SELECT MAX(amount) FROM transactions;  -- The maximum value in the amount column of the transactions table.
+
+SELECT MIN(amount) FROM transactions; -- The minimum value in the amount column of the transactions table.
+
+SELECT AVG(amount) FROM transactions;  -- Average (mean) of the values in a column.
+
+SELECT SUM(amount) FROM transactions;  -- The sum of all values in a column.
+
+SELECT CONCAT(first_name, " ", last_name) FROM customers AS  full_name;  -- Concatenate two columns in a table, in this case gegerating full_name column from first_name and last_name columns.
+
+
+
+
+
+-- Logical Operations in mySQL
+
+
+CREATE TABLE coworker(
+    
+    coworker_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    job VARCHAR(50),
+    hourly_pay DECIMAL(5, 2),
+    hire_date DATETIME DEFAULT NOW()
+    
+);
+
+INSERT INTO coworker (first_name, last_name, job, hourly_pay)
+VALUES ("Loqman", "Samani", "bioinformatician", 100),
+       ("Jaqub", "Badpar", "IT-specialist", 60),
+       ("Diman", "Hissaini", "teacher", 40),
+       ("Shaida", "Yosefi", "biologist", 70),
+       ("Somaye", "Mariwani", "lawyer", 80);
+       
+
+
+SELECT * FROM coworker
+WHERE job = "lawyer" AND hire_date > "2023-01-02";  -- Select all lawer which got hired after a specific date(how to use AND statement).
+
+
+SELECT * FROM coworker
+WHERE job = "IT-specialist" OR job = "teacher"; -- How to use OR statement.
+
+
+SELECT * FROM coworker
+WHERE NOT job = "bioinformatician"; -- How to use NOT statement.
+
+
+
+SELECT * FROM coworker
+WHERE NOT job = "bioinformatician" AND NOT job = "IT-specialist" ; -- How to combine AND & NOT statements.
+
+
+SELECT * FROM coworker
+WHERE hire_date BETWEEN "2022-02-01" AND "2024-08-09";  -- How to use BETWEEN operator.
+
+
+
+SELECT * FROM coworker
+WHERE job IN ("IT-specialist", "biologist", "doctor");  -- How to use IN operator.
+
+
+
+
+
+-- Wild card characters
+
+SELECT * FROM coworker
+WHERE first_name LIKE "S%";  -- All rows which the first_name  column starts with "S".
+
+SELECT * FROM coworker
+WHERE last_name LIKE "%ni"; -- All rows which the last_name  column ends with "ni".
+
+
+SELECT * FROM coworker
+WHERE job LIKE "_eacher";  -- "_" means it can be anything.
+
+
+SELECT * FROM coworker
+WHERE job LIKE "_ea%"; -- combine "_" and "%".
+
 
 
 
